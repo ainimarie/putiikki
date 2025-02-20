6fbc9db34433cfaf5218d76e3ce02ed1d963188a
ainimarie <ainimarie@gmail.com>
2025-02-21 00:36:15 +0200

    adds route for tasks

---
+++ server/src/routes/tasks.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+export const router = express.Router();
+import { getMultiple } from '../services/tasks';
+
+/* GET tasks listing. */
+router.get('/', function (req, res, next) {
+    console.log("asljkd");
+    try {
+        const pageNumber: number = typeof req.query.page === 'string' && parseInt(req.query.page);
+        console.log(getMultiple(1));
+        res.json(getMultiple(pageNumber));
+    } catch (err) {
+        console.error(`Error while getting tasks `, err.message);
+        next(err);
+    }
+});
